ade9e19ba5d2fe4514f3a088fa19362c0c2cb4d5
suan <abzal008@mail.ru>
2021-06-01 10:33:20 +0300

    add projects

--- src/Projects/Project/Project.tsx
+++ src/Projects/Project/Project.tsx
@@ -4,14 +4,15 @@ import style from './Project.module.css';
 type PropsType = {
     title: string
     description: string
+    style?: { color: string; backgroundImage: string; }
 }
 export const Project = (props: PropsType) => {
     return (
         <div className={style.project}>
-            <div className={style.img}>
+            <div style={props.style} className={style.img}>
                 <a href="">смотреть</a>
             </div>
-            <div className={style.description}>
+            <div className={style.descriptionBlock}>
                 <h3 className={style.title}>{props.title}</h3>
                 <span className={style.description}>{props.description}</span>
             </div>
--- src/Projects/Projects.tsx
+++ src/Projects/Projects.tsx
@@ -3,8 +3,15 @@ import style from './Projects.module.css';
 import containerStyle from './../Common/common-css-styles.module.css'
 import { Project } from './Project/Project';
 import { Title } from '../Common/components/Title/Title';
+import todoimg from './../assets/images/Без названия.png'
+import socislNetworkImg from './../assets/images/images.png'
 
 export const Projects = () => {
+
+    const todoImg = {
+        color: 'blue',
+        backgroundImage: `url(${todoimg})`,
+    };
 
     const description = [
         "fjsdlfjsdafjsdlfjsdlfjf;",
@@ -16,7 +23,7 @@ export const Projects = () => {
             <div className={`${containerStyle.container} ${style.projectsContainer}`}>
                 <Title text={"Projects"} />
                 <div className={style.projects}>
-                    <Project title="HTML" description={description[0]} />
+                    <Project style={todoImg} title="HTML" description={description[0]} />
                     <Project title="CSS" description={description[1]} />
                     <Project title="JS" description={description[2]} />
                     <Project title="JS" description={description[2]} />
