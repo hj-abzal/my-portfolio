68d352cc76a68b654beaad449efb18d3ce30637a
suan <abzal008@mail.ru>
2021-06-03 17:32:06 +0300

    :

--- src/AboutMe/AboutMe.tsx
+++ src/AboutMe/AboutMe.tsx
@@ -9,16 +9,31 @@ export const AboutMe: React.FC<AboutMePropsType> = () => {
 
     return (
         <div className={style.AboutMeBlock}>
-            <div className={style.descriptios}>
-                <div className={style.title}>
-                    <h2 className={style.aboutMe} >About me</h2>
+            <div className={style.container}>
+                <Title text={"About me"} />
+                <div className={style.description}>
+                    <div className={style.descText}>
+                        <p> I’m Fidalgo , a 23 years old self-taught Front-end developer , from Portugal .
+
+                        After my college degree in "Communication & Multimedia", I got a job as a "developer", where I stayed for almost 2 years, but I only developed websites with Wordpress custom themes, and HTML & CSS… So I wasn’t learning and improving, I felt stuck.
+                    </p>
+                    </div>
+
                 </div>
-                <p> I am fully commited and passioned about programming.
-                  Future is in our hand, literly we create can create new history through our code.</p>
-            </div>
-            <div className={style.code}>
+                <div className={style.code}>
+                    <div className={style.editor}>
+                        <div className={style.header}></div>
+                        <div className={style.codeText}></div>
+                    </div>
+                    <div className={style.result}>
+                        <p className={style.resultText}></p>
+                    </div>
+                </div>
+
+
 
             </div>
+
         </div>
     )
 }
