4b97524222810ddd1d7c3590fdceaf9a726fec31
suan <abzal008@mail.ru>
2021-06-05 00:51:28 +0300

    finished firm version

--- src/ContactsFooterC/Contacts/Contacts.tsx
+++ src/ContactsFooterC/Contacts/Contacts.tsx
@@ -1,22 +1,24 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import SuperButton from '../../Common/components/c2-SuperButton/SuperButton';
 import { Title } from '../../Common/components/Title/Title';
 import { AppStateType } from '../../Redux/Store';
 import style from './Contacts.module.scss';
 
 export const Contacts = () => {
-    const isLight = useSelector<AppStateType, boolean>(state => state.portfolio.isLight)
-    const inEnglish = useSelector<AppStateType, boolean>(state => state.portfolio.inEnglish)
+
 
     return (
         <div className={style.contactsBlock}>
             <div className={style.container}>
                 <Title text={'Contacts'} />
                 <form className={style.contactForm}  >
-                    <input type="text" />
-                    <input type="text" />
+                    <input className={style.inputStyle} type="text" />
+                    <input className={style.inputStyle} type="text" />
                     <textarea />
-                    <button type="submit" className={style.submitBtn}>Отправить</button>
+                    <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
+                        <SuperButton type={"submit"} className={style.submitBtn}> Отправить </SuperButton>
+                    </div>
                 </form>
 
             </div>
--- src/Footer/Footer.tsx
+++ src/Footer/Footer.tsx
@@ -2,17 +2,26 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { AppStateType } from '../Redux/Store';
 import style from './Footer.module.scss';
-import { Item } from './Items/Item';
+import insImg from '../assets/images/instagram.png'
+import telegramImg from '../assets/images/telegram.png'
+import linkedInImg from '../assets/images/linkedin.png'
+
 
 export const Footer = () => {
-  
+
     return (
         <div className={style.footer}>
             <h3>SUAN</h3>
             <div className={style.items}>
-                <Item title="inst" />
-                <Item title="VK" />
-                <Item title="HH" />
+                <div className={style.inst}>
+                    <img className={style.instImg} src={`${insImg}`} alt="" />
+                </div>
+                <div className={style.linkedIn}>
+                    <img className={style.linkedInImg} src={`${linkedInImg}`} alt="" />
+                </div>
+                <div className={style.tele}>
+                    <img className={style.teleImg} src={`${telegramImg}`} alt="" />
+                </div>
             </div>
             <p>© 2021 Copyright: Abzal Kozhakhmet</p>
         </div>
