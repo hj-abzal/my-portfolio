1a32752f9da073c3df0707f30341331f8efa0ab7
suan <abzal008@mail.ru>
2021-08-13 16:21:30 +0300

    add planktonics and fix fading

--- src/Projects/Projects.tsx
+++ src/Projects/Projects.tsx
@@ -4,10 +4,10 @@ import { Project } from './Project/Project';
 import { Title } from '../Common/components/Title/Title';
 import todoimg from './../assets/images/Без названия.png'
 import socislNetworkImg from './../assets/images/socialNetwork.jpg'
-import counterIMG from './../assets/images/first.png'
+import marvelIMG from './../assets/images/first.png'
 import MountainsImg from './../assets/images/second.jpg'
-//@ts-ignore
-import Fade from 'react-reveal/Fade';
+import planktonicsPng from './../assets/images/planktonics.png'
+
 
 export const Projects = () => {
 
@@ -19,26 +19,31 @@ export const Projects = () => {
         color: 'blue',
         backgroundImage: `url(${socislNetworkImg})`,
     };
-    const counterImg = {
+    const marverlImg = {
         color: 'blue',
-        backgroundImage: `url(${counterIMG})`,
+        backgroundImage: `url(${marvelIMG})`,
     };
     const futureProjectImg = {
         color: 'blue',
         backgroundImage: `url(${MountainsImg})`,
     };
+    const planktonicsStyle = {
+        color: 'blue',
+        backgroundImage: `url(${planktonicsPng})`,
+    };
 
     const description = [
         "TS, Redux, RestAPI, Thunk,Material UI, Unit Tests",
         "TS, Redux, RestAPI, Thunk,ANT Design,  Unit Tests",
-        "TS, Redux, RestAPI, Thunk,Material UI, Unit Tests",
+        "TS, Redux, Thunk,Material UI, React-router-dom",
         "...in progress",
     ];
     const linkName = [
         "https://hj-abzal.github.io/todolist/",
         "https://hj-abzal.github.io/social-network/",
         "https://hj-abzal.github.io/marvelHeroes/",
-        "...in progress",
+        "https://github.com/hj-abzal",
+        "https://hj-abzal.github.io/TOT-Systems-network"
     ]
 
 
@@ -47,14 +52,12 @@ export const Projects = () => {
             <div id="Projects" className={style.projectsContainer}>
                 <Title text={"Projects"} />
                 <div className={style.projects}>
-                    <Fade left>
-                        <Project linkName={linkName[0]} style={todoImg} title="TODOLIST" description={description[0]} />
-                        <Project linkName={linkName[1]} style={socislNetwork} title="THE SOCIAL NETWORK" description={description[1]} />
-                    </Fade>
-                    <Fade right>
-                        <Project linkName={linkName[2]} style={counterImg} title="MARVEL FUN APP" description={description[2]} />
-                        <Project style={futureProjectImg} title="FUTURE PROJECT" description={description[3]} />
-                    </Fade>
+                    <Project linkName={linkName[0]} style={todoImg} title="TODOLIST" description={description[0]} />
+                    <Project linkName={linkName[1]} style={socislNetwork} title="THE SOCIAL NETWORK" description={description[1]} />
+                    <Project linkName={linkName[2]} style={marverlImg} title="MARVEL FUN APP" description={description[2]} />
+                    <Project linkName={linkName[4]} style={planktonicsStyle} title="PlANKTONICS MESSANGER" description={description[2]} />
+                    <Project linkName={linkName[3]} style={futureProjectImg} title="FUTURE PROJECT" description={description[3]} />
+                    <Project linkName={linkName[3]} style={futureProjectImg} title="FUTURE PROJECT" description={description[3]} />
                 </div>
             </div>
         </div>
--- src/Skills/Skills.tsx
+++ src/Skills/Skills.tsx
@@ -12,7 +12,7 @@ library.add(fas);
 export const Skills = () => {
 
     const description = [
-        'React, Redux, Middlewere etc',
+        'React, Redux Toolkit, RestAPI',
         'JavaScript, TypeScript, HTML5, CSS3',
         'Unit Tests, SnapShot, Storybook',
         'Material UI, Ant-Design etc',
@@ -23,34 +23,30 @@ export const Skills = () => {
             <div className={style.skillsContainer}>
                 <Title text={"Skills"} />
                 <div className={style.skills}>
-                    <Fade left>
 
-                        <Skill
-                            icon={['fas', 'laptop-code']}
-                            title={'DEVELOPMENT'}
-                            description={description[0]}
-                        />
+                    <Skill
+                        icon={['fas', 'laptop-code']}
+                        title={'DEVELOPMENT'}
+                        description={description[0]}
+                    />
 
-                        <Skill
-                            icon={['fas', 'code']}
-                            title={'FOUNDATION'}
-                            description={description[1]}
-                        />
-                    </Fade>
+                    <Skill
+                        icon={['fas', 'code']}
+                        title={'FOUNDATION'}
+                        description={description[1]}
+                    />
 
-                    <Fade right>
-                        <Skill
-                            icon={['fas', 'clipboard-list']}
-                            title={'TESTING'}
-                            description={description[2]}
-                        />
+                    <Skill
+                        icon={['fas', 'clipboard-list']}
+                        title={'TESTING'}
+                        description={description[2]}
+                    />
 
-                        <Skill
-                            icon={['fab', 'sketch']}
-                            title={'DESIGN'}
-                            description={description[3]}
-                        />
-                    </Fade>
+                    <Skill
+                        icon={['fab', 'sketch']}
+                        title={'DESIGN'}
+                        description={description[3]}
+                    />
                 </div>
             </div>
         </div>
