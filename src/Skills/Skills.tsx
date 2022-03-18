5bfc0d4cdba0a28fad40a200827341c31225f3ba
hj-abzal <171-421@mail.ru>
2022-03-19 01:07:48 +0600

    add skill and renew cv

--- src/Skills/Skills.tsx
+++ src/Skills/Skills.tsx
@@ -1,56 +1,61 @@
 import React from 'react';
 import style from './Skills.module.scss';
-import { Skill } from './Skill/Skill';
-import { Title } from '../common/components/Title/Title';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { fas } from '@fortawesome/free-solid-svg-icons';
-
+import {Skill} from './Skill/Skill';
+import {Title} from '../common/components/Title/Title';
+import {library} from '@fortawesome/fontawesome-svg-core';
+import {fas} from '@fortawesome/free-solid-svg-icons';
 
 
 library.add(fas);
 export const Skills = () => {
 
-    const description = [
-        'React, Redux Toolkit, RestAPI',
-        'JavaScript, TypeScript, HTML5, CSS3',
-        'Unit Tests, SnapShot, Storybook',
-        'Material UI, Ant-Design etc',
-        'Node.js, Express, MongoDB'
-    ];
+    const description = {
+        frontend: 'React(redux), Angular(Ngrx)',
+        foundation: 'JavaScript, TypeScript, HTML5, CSS3, Git',
+        testing: 'Unit Tests, SnapShot, Storybook',
+        ui: 'Material UI, Ant-Design etc, Angular Material',
+        backend: 'Node.js, Express, NestJS, Docker',
+        database: 'Postgresql, MongoDB'
+    };
 
     return (
         <div id="Skills" className={style.skillsBlock}>
             <div className={style.skillsContainer}>
-                <Title text={"Skills"} />
+                <Title text={"Skills"}/>
                 <div className={style.skills}>
 
                     <Skill
                         icon={['fas', 'laptop-code']}
-                        title={'DEVELOPMENT'}
-                        description={description[0]}
+                        title={'FRONTEND'}
+                        description={description.frontend}
                     />
 
                     <Skill
                         icon={['fas', 'code']}
                         title={'FOUNDATION'}
-                        description={description[1]}
+                        description={description.foundation}
                     />
 
                     <Skill
                         icon={['fas', 'clipboard-list']}
                         title={'TESTING'}
-                        description={description[2]}
+                        description={description.testing}
                     />
 
                     <Skill
                         icon={['fab', 'sketch']}
                         title={'DESIGN'}
-                        description={description[3]}
+                        description={description.ui}
                     />
                     <Skill
                         icon={['fas', 'server']}
                         title={'BACKEND'}
-                        description={description[4]}
+                        description={description.backend}
+                    />
+                    <Skill
+                        icon={['fas', 'database']}
+                        title={'Database'}
+                        description={description.database}
                     />
                 </div>
             </div>
